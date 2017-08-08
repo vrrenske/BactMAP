09c2692d2cc6af99809a8fa1b108ebfd2cad7ae1
vrrenske <r.van.raaphorst@rug.nl>
2017-08-08 16:10:28 +0200

    add spot function

--- R/ouftidirectlyfrommatlab.R
+++ R/ouftidirectlyfrommatlab.R
@@ -55,6 +55,7 @@ spotrInMatGetMesh <- function(cellList){
       for(z in 1:nrow(meshline)){
         if(z==1){
           meshline$length[z] <- 0
+          meshline$steplength[z] <- 0
         }
         if(z > 1){
           xdistL0 <- meshline$x0[z]-meshline$x0[z-1]
@@ -62,12 +63,21 @@ spotrInMatGetMesh <- function(cellList){
           distL0 <- sqrt(xdistL0^2 + ydistL0^2)
           angL0 <- atan(ydistL0/xdistL0)
           angd <- atan(ydist[z]/xdist[z])
-          anglength <- pi-angL0-angd
+          anglength <- pi-abs(angL0)-abs(angd)
           steplength <- sin(anglength)*distL0
-          meshline$length[z] <- steplength
+          meshline$steplength[z] <- steplength
+          meshline$length[z] <- sum(meshline$steplength[1:z])
+        }
+        if(z==max(meshline$num+1)){
+          angK <- atan(xdist[z-1]/ydist[z-1])
+          anglengthEnd <- (pi/2)-abs(angL0-abs(angK))
+          steplengthK <- sin(anglengthEnd)*distL0
+          meshline$steplength[z] <- steplengthK
+          meshline$length[z] <- sum(meshline$steplength[1:z])
+
         }
       }
-      meshline$max_length <- max(meshline$length)
+      meshline$max_length <- sum(meshline$steplength)
       if(n==1){MESH <- meshline}
       if(n>1){MESH <- rbind(MESH, meshline)}
     }
@@ -75,7 +85,35 @@ spotrInMatGetMesh <- function(cellList){
   return(MESH)
 }
 
+spotrInMatGetSpots <- function(cellList){
+  spots <- cellList$spots
+  u <- 1
+  for(n in 1:length(spots)){
+      l <- spots[[n]][1]
+      d <- spots[[n]][2]
+      x <- spots[[n]][3]
+      y <- spots[[n]][4]
+      position <- spots[[n]][5]
+      adj_Rsquared <- spots[[n]][6]
+      CI_xy <- spots[[n]][7]
+      spotframe <- data.frame("l"=t(l[[1]]), "d"=t(d[[1]]), "x"=t(x[[1]]), "y"=t(y[[1]]), "position" = t(position[[1]]), "adj_Rsquared" = t(adj_Rsquared[[1]]), "CI_xy" =paste(CI_xy[[1]]))
+      if(nrow(spotframe)>0){
+      spotframe$frame <- cellList$frame[n]
+      spotframe$cell <- cellList$cell[n]
+        if(u!=1){
+          spottotal <- rbind(spottotal, spotframe)
+        }
+        else{spottotal <- spotframe
+             u <- u+1}
+        }
+      }
+  return(spottotal)
+}
+
+
+
 matfile <- file.choose()
 cellList <- spotrInMatOufti(matfile)
+REP <- spotrInMatGetSpots(cellList)
 MESH <- spotrInMatGetMesh(cellList)
 
