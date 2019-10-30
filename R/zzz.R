dba38854412a75b4622cd045bdd77dcaf886682d
vrrenske <renske.vanraaphorst@unil.ch>
2019-10-30 17:36:51 +0100

    New version function check. Add startup message

---
+++ R/zzz.R
@@ -0,0 +1,6 @@
+##load instructions
+
+
+.onAttach <- function(libname,pkgname){
+  packageStartupMessage("Welcome to BactMAP version 0.1.0.1 (30 October 2019). \n see what's new, type '?bactmapnews' in your console.")
+}
